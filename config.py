a7840836e1ffa9229954b9437ad9ab6adfcedc2e
Arian <78182876+ArianFotouhi@users.noreply.github.com>
2023-08-22 12:08:28 -0400

    v4.3

---
+++ config.py
@@ -0,0 +1,16 @@
+secret_key = ''
+my_api_key = ""
+
+model_name = "gpt-3.5-turbo-0613"
+# model_name = "gpt-3.5-turbo"
+#model_name = 'gpt-3.5-turbo-16k'
+
+#only applicable in case of email notification
+sender_email_address = ''
+sender_email_password = ''
+recpeint_email_address = ''
+
+#only applicable in case of user authentication based app
+users = {
+    "user1":{"password":"pass", "first_name":"Edward", "last_name":"Morales","email":"morales421@yahoo.com"}
+}
