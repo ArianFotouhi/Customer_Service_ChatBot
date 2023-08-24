e0972e5509f7f56c06df60d0cfc4dd7d13df9652
Arian <78182876+ArianFotouhi@users.noreply.github.com>
2023-08-24 16:32:02 -0400

    v4.9.5

--- db_reader.py
+++ db_reader.py
@@ -1,11 +1,24 @@
 from bot_db import ChatDatabase, RequestDatabase
 
 db = ChatDatabase()
-#db = RequestDatabase()
+# db = RequestDatabase()
+
 db.create_table()
 
 
 #table_name = 'chat_history'
 #query = f"PRAGMA table_info({table_name})"
 
-db.fetch_table()
+output = db.fetch_table(query='''SELECT user_message, bot_response, datetime
+FROM chat_history
+ORDER BY datetime DESC
+LIMIT 20;
+''')
+                        
+history = []
+for i in output:
+
+    # print(i[0])
+    history.append({'user': i[0], 'customer_service_bot': i[1], 'datetime': i[2]})
+
+print(history)
